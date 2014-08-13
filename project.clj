093c0c37d165b94705e76aa1dc9be1d0af77da76
maxi-k <maxi.kuschewski@gmail.com>
2014-08-13 02:02:14 +0200

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2202"]
-                 [org.clojure/core.async "0.1.303.0-886421-alpha"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2311"]
+                 [org.clojure/core.async "0.1.319.0-6b1aca-alpha"]
                  [hiccups "0.3.0"]]
   :plugins [[lein-cljsbuild "1.0.3"]]
   :source-paths ["src/cljs"]
